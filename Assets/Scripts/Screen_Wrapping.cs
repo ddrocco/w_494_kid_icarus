48ecfdc2f88fc21418767dfad8f84e8144b99ce6
ddrocco <keredoccor@gmail.com>
2015-01-19 17:56:42 -0500

    Added dynamic world construction and folders

---
+++ Assets/Scripts/Screen_Wrapping.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screen_Wrapping : MonoBehaviour {
+	private Renderer[] renderers;
+	private bool isWrappingX = false;
+	private float screenWidth;
+	private float screenHeight;
+	private Transform[] ghosts = new Transform[2];
+
+	// Use this for initialization
+	void Start () {
+		renderers = GetComponentsInChildren<Renderer>();
+		Vector3 screenBottomLeft = Camera.main.ViewportToWorldPoint (new Vector3 (0, 0, transform.position.z));
+		Vector3 screenTopRight = Camera.main.ViewportToWorldPoint (new Vector3 (1, 1, transform.position.z));
+		screenWidth = screenTopRight.x - screenBottomLeft.x;
+		screenHeight = screenTopRight.y - screenBottomLeft.y;
+		CreateGhosts ();
+		PositionGhosts ();
+	}
+	
+	bool CheckRenderers() {
+		foreach (Renderer renderer in renderers) {
+			if(renderer.isVisible) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	void CreateGhosts() {
+		for (int i = 0; i < 2; ++i) {
+			ghosts[i] = Instantiate (transform, Vector3.zero, Quaternion.identity) as Transform;
+			DestroyImmediate(ghosts[i].GetComponent<Screen_Wrapping>());
+		}
+	}
+	
+	void PositionGhosts() {
+		Vector3 ghostPosition = transform.position;
+		
+		//right ghost
+		ghostPosition.x = transform.position.x + screenWidth;
+		ghostPosition.y = transform.position.y;
+		ghosts [0].position = ghostPosition;
+		
+		//left ghost
+		ghostPosition.x = transform.position.x - screenWidth;
+		ghostPosition.y = transform.position.y;
+		ghosts [1].position = ghostPosition;
+		
+		for (int i = 0; i < 2; ++i) {
+			ghosts[i].rotation = transform.rotation;
+		}
+	}
+	
+	void Swap() {
+		foreach (Transform ghost in ghosts) {
+			if (ghost.position.x < screenWidth &&
+			    ghost.position.x > -screenWidth &&
+			    ghost.position.y < screenHeight &&
+			    ghost.position.y > -screenHeight)
+			{
+				transform.position = ghost.position;
+				break;
+			}
+		}
+		PositionGhosts ();
+	}
+	
+	void ScreenWrap() {
+		if (CheckRenderers ()) {
+			isWrappingX = false;
+			return;
+		}
+		if (isWrappingX) {
+			return;
+		}
+		Vector3 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
+		Vector3 newPos = transform.position;
+		if (!isWrappingX && (viewportPos.x > 1 || viewportPos.x < 0)) {
+			newPos.x = -newPos.x;
+			isWrappingX = true;
+		}
+		transform.position = newPos;
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+		ScreenWrap ();
+		Swap ();
+	}
+}
---
+++ Assets/Scripts/World_Generator.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class World_Generator : MonoBehaviour {
+	public class Vector16 {
+		public List<int> get;
+		public Vector16(int a, int b, int c, int d, int e, int f, int g,
+				int h, int i, int j, int k, int l,  int m, int n,
+				int o, int p) {
+			get = new List<int>();
+			get.Add (a);
+			get.Add (b);
+			get.Add (c);
+			get.Add (d);
+			get.Add (e);
+			get.Add (f);
+			get.Add (g);
+			get.Add (h);
+			get.Add (i);
+			get.Add (j);
+			get.Add (k);
+			get.Add (l);
+			get.Add (m);
+			get.Add (n);
+			get.Add (o);
+			get.Add (p);
+		}
+	}
+
+	public List<Vector16> world;
+	public List<GameObject> worldBlocks;
+	// Use this for initialization
+	
+	void Awake() {
+		world = new List<Vector16>();
+		world.Add(new Vector16(0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 0));
+		world.Add(new Vector16(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+	}
+	
+	void Start() {
+		int rows = world.Count;
+		print (rows);
+		for (int y = 0; y < rows; ++y) {
+			for (int x = 0; x < 16; ++x) {
+				Build(world[y].get[x], x, rows - y);
+			}
+		}
+	}
+	
+	void Build(int ID, int x, int y) {
+		if (ID == 9) {
+			return;
+		}
+		GameObject newObject = Instantiate (worldBlocks[ID]) as GameObject;
+		newObject.transform.position = new Vector3(x - 8, y, 0);
+		newObject.transform.parent = transform;
+	}
+}
