ba6a2151c8bd657bc5d878f8c8e757df07fbba7b
Maia Hoberman <maiayh@umich.edu>
2015-01-20 00:03:32 -0500

    What Maia was working on...

--- Assets/Scripts/Foe_Eye.cs
+++ Assets/Scripts/Foe_Eye.cs
@@ -4,21 +4,44 @@ using System.Collections.Generic;
 
 public class Foe_Eye : MonoBehaviour {
 	public GameObject eye;
-	private Transform[] eyes = new Transform[4];
-	public float speed = 3f;
+	private Vector3 spawnPoint;
+	//private Transform[] eyes = new Transform[3];
+	public float mSpeed = 1f;
+	public float mXScale = 1f;
+	public float mYScale = 1f;
+	
+	private Vector3 mPivot;
+	private Vector3 mPivotOffset;
+	private float mPhase;
+	private bool mInvert = false;
+	private float m2Pi = Mathf.PI * 2;
 	
 	// Use this for initialization
-	void Start () {
-		Vector3 spawnPoint = Camera.main.ViewportToWorldPoint (new Vector3 (.25f, 1, 0));
-		for (int i = 0; i < 4; ++i) {
-			eyes[i] = Instantiate(eye, spawnPoint, Quaternion.identity) as Transform;
-		}
+	void Awake () {
+		Vector3 screenTopLeft = Camera.main.ViewportToWorldPoint (new Vector3 (.25f, 1, 0));
+		spawnPoint = new Vector3(screenTopLeft.x, screenTopLeft.y, 0);
+	}
+	
+	void Start() {
+		mPivot = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		for (int i = 0; i < 4; ++i) {
-			eyes[i].transform.Translate (new Vector3(0, -speed, 0));
+		mPivotOffset = Vector3.up * 2 * mXScale;
+		mPhase += mSpeed * Time.deltaTime;
+		if (mPhase > m2Pi) {
+			mInvert = !mInvert;
+			mPhase -= m2Pi;
 		}
+		if (mPhase < 0) {
+			mPhase += m2Pi;
+		}
+		
+		transform.position = mPivot + (mInvert ? mPivotOffset : Vector3.zero);
+		Vector3 newPos = transform.position;
+		newPos.y += Mathf.Sin (mPhase) * mYScale;
+		newPos.x += Mathf.Cos (mPhase) * (mInvert ? -1 : 1) * mXScale;
+		transform.position = newPos;
 	}
 }
