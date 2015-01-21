67e0e3714d773407e59454b6b234fcb60a2001bf
ddrocco <keredoccor@gmail.com>
2015-01-20 22:11:26 -0500

    Beautification!

---
+++ Assets/Prefabs/WorldBlocks/BlockSpawnDisplace.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockSpawnDisplace : MonoBehaviour {
+	public float displacement;
+	
+	// Use this for initialization
+	void Start () {
+		transform.Translate(Vector3.up * displacement);
+	}
+}
